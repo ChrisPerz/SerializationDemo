9b62d56eee5949abd11a0eb64d7477cd58f6d778
Christian Perez <cristianperez028@gmail.com>
2025-06-04 23:06:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+public class Person
+{
+    public string UserName { get; set; }
+    public int UserAge { get; set; }
+}
+
+public class Program{
+    public static void Main(string[] args){
+        Person testPerson = new Person{
+            UserName = "John Doe",
+            UserAge = 30
+        };
+
+        using( FileStream fs = new FileStream("person.dat", FileMode.Create))
+        {
+            BinaryWriter writer = new BinaryWriter(fs);
+            writer.Write(testPerson.UserName);
+            writer.Write(testPerson.UserAge);
+            Console.WriteLine("Finished Serialization to binary");
+        }   
+        
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+        using( StreamWriter streamWriter = new StreamWriter("person.xml"))
+        {
+            xmlSerializer.Serialize(streamWriter, testPerson);
+            Console.WriteLine("Finished Serialization to XML");
+        }
+
+       
+    }
+}
